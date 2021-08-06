25cf2d5e3b8e25b4176150cf1a2bdc33f7b8a015
pbegle <peter.begle@bytecode.io>
2021-08-06 16:08:54 -0700

    Consolidated errors into client module, refactored to add backoff

--- tap_dixa/client.py
+++ tap_dixa/client.py
@@ -1,9 +1,83 @@
 import base64
 from enum import Enum
 
-from requests import Session
+import backoff
+import requests
+from singer import get_logger
 
-from tap_dixa.errors import raise_for_error
+LOGGER = get_logger()
+
+
+class DixaclientError(Exception):
+    def __init__(self, message=None, response=None):
+        super().__init__(message)
+        self.message = message
+        self.response = response
+
+
+class DixaClient5xxError(DixaclientError):
+    pass
+
+
+class DixaClient401Error(DixaclientError):
+    pass
+
+
+class DixaClient400Error(DixaclientError):
+    pass
+
+
+class DixaClient429Error(DixaclientError):
+    pass
+
+
+ERROR_CODE_EXCEPTION_MAPPING = {
+    500: {
+        'raise_exception': DixaClient5xxError,
+        'message': 'Server Error',
+    },
+    503: {
+        'raise_exception': DixaClient5xxError,
+        'message': 'Server Error',
+    },
+    401: {
+        'raise_exception': DixaClient401Error,
+        'message': 'Invalid or missing credentials'
+    },
+    400: {
+        'raise_exception': DixaClient400Error,
+        'message': 'Invalid query parameters'
+    },
+    429: {
+        'raise_exception': DixaClient429Error,
+        'message': 'API limit has been reached'
+    },
+}
+
+
+def raise_for_error(resp):
+    try:
+        resp.raise_for_status()
+    except (requests.HTTPError, requests.ConnectionError) as error:
+        try:
+            error_code = resp.status_code
+            client_exception = ERROR_CODE_EXCEPTION_MAPPING.get(error_code, {})
+            exc = client_exception.get('raise_exception', DixaclientError)
+            message = client_exception.get('message', 'Client Error')
+
+            raise exc(message, resp) from None
+
+        except (ValueError, TypeError):
+            raise DixaclientError(error) from None
+
+
+def retry_after_wait_gen():
+    while True:
+        # This is called in an except block so we can retrieve the exception
+        # and check it.
+        sleep_time = 60
+        LOGGER.info("API rate limit exceeded -- sleeping for %s seconds", sleep_time)
+        yield sleep_time
 
 
 class DixaURL(Enum):
@@ -16,7 +90,7 @@ class Client:
     def __init__(self, api_token: str):
         self._api_token = api_token
         self._base_url = None
-        self._session = Session()
+        self._session = requests.Session()
         self._headers = {}
 
     @staticmethod
@@ -42,6 +116,7 @@ class Client:
     def _post(self, url, headers=None, params=None, data=None):
         return self._make_request(url, method='POST', headers=headers, params=params, data=data)
 
+    @backoff.on_exception(retry_after_wait_gen, DixaClient429Error, jitter=None, max_tries=3)
     def _make_request(self, url, method, headers=None, params=None, data=None):
 
         with self._session as session:
