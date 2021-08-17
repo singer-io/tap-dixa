066cccb396621f477ea0e9399d552caaed108788
pbegle <peter.begle@bytecode.io>
2021-08-16 17:51:17 -0700

    Added unittests.

--- tap_dixa/helpers.py
+++ tap_dixa/helpers.py
@@ -6,11 +6,11 @@ import pytz
 
 
 def unix_ms_to_date(ms: int) -> datetime.datetime:
-    return datetime.datetime.fromtimestamp(ms/1000, pytz.UTC).isoformat()
+    return datetime.datetime.fromtimestamp(ms/1000).replace(microsecond=0).isoformat()
 
 
 def datetime_to_unix_ms(datetime_obj: datetime.datetime) -> int:
-    return int(time.mktime(datetime_obj.timetuple()) * 1000)
+    return int(datetime_obj.timestamp() * 1000)
 
 
 def create_csid_params(csids: Iterator) -> dict:
