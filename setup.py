0dd81dc6718e382635ea9a2752811cff0e0ef3d0
Andy Lu <andy@stitchdata.com>
2021-10-04 20:20:18 +0000

    Allow upload to PyPI

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ from setuptools import find_packages, setup
 
 requirements_file = "requirements.txt"
 
-with open("README.md") as f:
-    readme = f.read()
-
 setup(
     name="tap-dixa",
     version="0.1.0",
     description="Singer.io tap for extracting data from the Dixa API",
-    long_description=readme,
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
