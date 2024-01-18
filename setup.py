ed8533f978793c9981622b5f7643810885fd9d93
Ubuntu <ubuntu@ip-10-10-11-197.ec2.internal>
2024-01-18 19:47:58 +0000

    Bump urllib3 for SCA

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "simplejson==3.11.1",
         "singer-python==5.12.1",
         "six==1.16.0",
-        "urllib3==1.26.6",
+        "urllib3==1.26.18",
     ],
     entry_points="""
     [console_scripts]
