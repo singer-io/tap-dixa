1a72a416a06003e2f8332ad72ce66829b0c4ccf5
Leslie VanDeMark <lvandemark@talend.com>
2026-06-10 13:32:42 +0000

    bump version to 1.0.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements_file = "requirements.txt"
 
 setup(
     name="tap-dixa",
-    version="1.0.9",
+    version="1.0.10",
     description="Singer.io tap for extracting data from the Dixa API",
     author="Stitch",
     url="http://singer.io",
